03087b04a9b666dae14aa0bde4a69c9f3b3259d1
homergarnet <100991264+homergarnet@users.noreply.github.com>
2023-09-23 12:46:02 +0800

    update environment.production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   googleMapsApiKey: 'AIzaSyCDqwAgErq1u7LfsyvdRVm14Fn7baGH1vU',
+  imgNoImageDisplay: '/assets/images/no-image.png',
   apiUrl: 'http://localhost:8001/',
 };
